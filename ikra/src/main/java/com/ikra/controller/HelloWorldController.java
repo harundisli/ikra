f6bda80e300b4936ebb665687cb977e089f43473
harundisli <harundisli@gmail.com>
2020-04-09 18:57:21 +0300

    Authentication özelliği eklendi. DB bağlantısı yapıldı.

---
+++ ikra/src/main/java/com/ikra/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ikra.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloWorldController {
+    @RequestMapping({ "api/hello" })
+    public String hello() {
+        return "Hello World";
+    }
+}
